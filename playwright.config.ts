641c2be61c862eaf9cf8773b90c7f8f1486231a0
Bruno Nunes Garcia <109298177+bnunesgarcia@users.noreply.github.com>
2025-03-16 19:20:51 -0300

    Atualizacao de arquivo config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,11 +2,11 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  outputDir: './test-results',
+  reporter: [
+    ['html', { outputFolder: 'reports', open: 'never' }]
+  ],
   workers: 1,
-  reporter: 'html',
   use: {
     headless: true,
     trace: 'on-first-retry',
